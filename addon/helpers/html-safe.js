e02a42a2803cff09ee4271228e24251c567dfb7d
maxwondercorn <gmartell@pobox.com>
2022-07-29 21:54:05 -0400

    another fix

--- addon/helpers/html-safe.js
+++ addon/helpers/html-safe.js
@@ -1,7 +1,6 @@
 import { helper } from '@ember/component/helper';
-import { htmlSafe as _htmlSafe, isHTMLSafe } from '@ember/template';
+import { htmlSafe as _htmlSafe } from '@ember/template';
 
 export default helper(function htmlSafe(string /*, hash*/) {
-  const safeString = _htmlSafe(string);
-  return isHTMLSafe(safeString);
+  return _htmlSafe(string);
 });
