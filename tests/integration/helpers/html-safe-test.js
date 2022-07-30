eb1e826ab5fc72ee45c5d31d9d1d3af81792c403
maxwondercorn <gmartell@pobox.com>
2022-07-29 22:20:58 -0400

    adjust tests

--- tests/integration/helpers/html-safe-test.js
+++ tests/integration/helpers/html-safe-test.js
@@ -6,16 +6,19 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Helper | html-safe', function (hooks) {
   setupRenderingTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it renders', async function (assert) {
-    this.set('inputValue', '1234');
+  test('it renders styles', async function (assert) {
+    this.set('inputValue', 21);
 
-    await render(hbs`{{html-safe this.inputValue}}`);
+    await render(hbs`{{html-safe (concat "width: " this.inputValue)}}`);
 
-    assert.dom(this.element).hasText('1234');
+    assert.dom(this.element).hasText('width: 21');
+  });
 
-    this.set('inputValue', '<p>Hello</p>');
-    await render(hbs`{{html-safe this.inputValue}}`);
-    assert.dom(this.element).hasText('<p>Hello</p>');
+  test('it renders classes', async function (assert) {
+    this.set('inputValue', 'red');
+
+    await render(hbs`<div class="btn {{html-safe this.inputValue}}"></div>`);
+
+    assert.dom(this.element).hasText('width: 21');
   });
 });
