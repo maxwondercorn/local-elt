fe8867b6c8f149aa2669bd8f4067aea6b6cddfa2
maxwondercorn <gmartell@pobox.com>
2022-08-02 23:58:05 -0400

    fix spanned row name

--- addon/components/lt-spanned-row.js
+++ addon/components/lt-spanned-row.js
@@ -2,7 +2,7 @@ import Component from '@ember/component';
 import classic from 'ember-classic-decorator';
 
 @classic
-export default class LtSpannedComponent extends Component {
+export default class LtSpannedRowComponent extends Component {
   colspan = 1;
   tagName = '';
   visible = true;
